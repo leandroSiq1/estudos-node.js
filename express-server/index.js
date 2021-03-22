e37d6528ec4b61bda5cfea40ff1108f140cf9b8f
Leandro <le_kts12@hotmail.com>
2021-03-22 16:19:12 -0300

    ...

--- express-server/index.js
+++ express-server/index.js
@@ -1,8 +1,48 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
 
+// definindo os arquivos estaticos
+// const staticFolder = path.join(__dirname, 'views');
+// const expressStatic = express.static(staticFolder);
+// app.use(expressStatic);
+app.use(express.static(path.join(__dirname, 'views')));
+
+// definindo os arquivos publicos
+// const publicFolder = path.join(__dirname, 'public');
+// const expressPublic = express.static(publicFolder);
+// app.use(expressPublic);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+// rotas
+app.get('/', (req, res) => {
+  res.render('views/index');
+});
+
+app.get('/sobre', (req, res) => {
+  res.send("sobre");
+});
+
+
+
+
+
+
+// 404 error (not found)
+app.use((req, res) => { // middleware
+  res.send("Página não encontrada!")
+});
+
+
+
+
+
+// exec servidor
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Serve is listening on port ${port}`);
 });
